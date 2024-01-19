a5e39eb2d9aaa3061ef8db69d394c54718e7d231
Mukund Solanki <mukundsolanki665@gmail.com>
2024-01-19 21:50:59 +0530

    Build method for Bottom model sheet

--- vaachakapp/lib/pages/home_screen.dart
+++ vaachakapp/lib/pages/home_screen.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'widgets/bottom_model_sheet.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Home Screen'),
+    return Scaffold(
+      body: Center(
+        child: Text('Home Screen Content'),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            builder: (BuildContext context) => MyBottomSheet(),
+          );
+        },
+        label: Text('ADD SIGN'),
+        icon: Icon(Icons.edit),
+      ),
     );
   }
 }
