5c4d5dd1f1bffcbd2359975943e46a9191e6f60b
Mukund Solanki <mukundsolanki665@gmail.com>
2024-01-20 00:23:45 +0530

    sample for connection screen

--- vaachakapp/lib/pages/connect_screen.dart
+++ vaachakapp/lib/pages/connect_screen.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'widgets/call.dart';
 
 class ConnectScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Connect Screen'),
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Lottie.asset(
+              'assets/lottie/call.json',
+              width: 400,
+            ),
+            // Start Call Button
+            ElevatedButton.icon(
+              onPressed: () {
+                // Call ID is used to establish connection between two diifferent users
+                Navigator.push(context, MaterialPageRoute(builder: (context) => const CallPage(),),);
+              },
+              icon: Icon(Icons.video_call),
+              label: Text('Start Call'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
