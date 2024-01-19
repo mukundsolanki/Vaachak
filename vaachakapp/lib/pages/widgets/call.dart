0240dfea53dd8061d96b4cb3f5302b5b3e0e91a0
Mukund Solanki <mukundsolanki665@gmail.com>
2024-01-19 23:09:41 +0530

    Agora UI kit

--- vaachakapp/lib/pages/widgets/call.dart
+++ vaachakapp/lib/pages/widgets/call.dart
@@ -1,20 +1,65 @@
-// import 'package:flutter/material.dart';
-// import 'package:zego_uikit_prebuilt_call/zego_uikit_prebuilt_call.dart';
+import 'package:agora_uikit/agora_uikit.dart';
+import 'package:flutter/material.dart';
 
-// class CallPage extends StatelessWidget {
-//   const CallPage({Key? key, required this.callID}) : super(key: key);
-//   final String callID;
+const appId = 'a601fc2a6ca742bea9879b723e4a3ab5';
+String channelName = 'vaachak_channel';
+String token =
+    '007eJxTYNg9z+nJhY37d7bVtgnPLXnH9+xp7/mFcv8++3DkTFg6/5OFAkOimYFhWrJRollyormJUVJqoqWFuWWSuZFxqkmicWKSaUZ8R2pDICNDVeweJkYGCATx+RnKEhOTMxKz44FEXl5qDgMDAADbJxU=';
+int uid = 0;
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return ZegoUIKitPrebuiltCall(
-//       appID: 1511202084, // Fill in the appID that you get from ZEGOCLOUD Admin Console.
-//       appSign: "2bb52911555aaf3cef7a7477a88f59b39c9459fd91ada7e0a064521df8841fd1", // Fill in the appSign that you get from ZEGOCLOUD Admin Console.
-//       userID: 'user1',
-//       userName: 'user_name1',
-//       callID: callID,
-//       // You can also use groupVideo/groupVoice/oneOnOneVoice to make more types of calls.
-//       config: ZegoUIKitPrebuiltCallConfig.oneOnOneVideoCall(),
-//     );
-//   }
-// }
+class CallPage extends StatefulWidget {
+  const CallPage({Key? key}) : super(key: key);
+
+  @override
+  State<CallPage> createState() => _CallPageState();
+}
+
+class _CallPageState extends State<CallPage> {
+  final AgoraClient client = AgoraClient(
+    agoraConnectionData: AgoraConnectionData(
+      appId: appId,
+      channelName: channelName,
+      tempToken: token,
+      uid: uid,
+    ),
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    initAgora();
+  }
+
+  void initAgora() async {
+    await client.initialize();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Speech Therapist'),
+          centerTitle: true,
+        ),
+        body: SafeArea(
+          child: Stack(
+            children: [
+              AgoraVideoViewer(
+                client: client,
+                layoutType: Layout.floating,
+                enableHostControls: true,
+              ),
+              AgoraVideoButtons(
+                client: client,
+                onDisconnect: () => Navigator.pop(
+                  context,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
