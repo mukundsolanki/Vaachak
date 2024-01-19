77953a3a60c2de9d7699d0dc91dc70ea689b81a1
Mukund Solanki <mukundsolanki665@gmail.com>
2024-01-19 22:17:19 +0530

    Adding new signs to home page

--- vaachakapp/lib/pages/widgets/bottom_model_sheet.dart
+++ vaachakapp/lib/pages/widgets/bottom_model_sheet.dart
@@ -1,35 +1,48 @@
 import 'package:flutter/material.dart';
 
-class MyBottomSheet extends StatelessWidget {
+class MyBottomSheet extends StatefulWidget {
+  final Function(String) onTextCreated;
+
+  MyBottomSheet({required this.onTextCreated});
+
+  @override
+  _MyBottomSheetState createState() => _MyBottomSheetState();
+}
+
+class _MyBottomSheetState extends State<MyBottomSheet> {
+  TextEditingController textEditingController = TextEditingController();
+
+  @override
+  void dispose() {
+    textEditingController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    TextEditingController textEditingController = TextEditingController();
-
-    return SizedBox(
-      height: 350.0,
-      child: Container(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            TextField(
-              controller: textEditingController,
-              decoration: InputDecoration(
-                labelText: 'Enter your text',
-              ),
+    return Container(
+      height: 400,
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          TextField(
+            controller: textEditingController,
+            decoration: InputDecoration(
+              labelText: 'Enter your text',
             ),
-            SizedBox(height: 16.0),
-
-            ElevatedButton(
-              onPressed: () {
-                String enteredText = textEditingController.text;
-                print('Creating with text: $enteredText');
-                Navigator.pop(context);
-              },
-              child: Text('Create'),
-            ),
-          ],
-        ),
+          ),
+          SizedBox(height: 16.0),
+          ElevatedButton(
+            onPressed: () {
+              String enteredText = textEditingController.text;
+              widget.onTextCreated(enteredText); // Callback to pass text to HomeScreen
+              textEditingController.clear(); // Clear the text field
+              Navigator.pop(context);
+            },
+            child: Text('Create'),
+          ),
+        ],
       ),
     );
   }
