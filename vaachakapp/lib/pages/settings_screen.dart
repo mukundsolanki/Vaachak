2fd224437519451b195b8350969a0bcec3969109
Mukund Solanki <mukundsolanki665@gmail.com>
2024-01-20 09:27:06 +0530

    Adding dialog to select language

--- vaachakapp/lib/pages/settings_screen.dart
+++ vaachakapp/lib/pages/settings_screen.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'states/app_provider.dart';
+import 'package:http/http.dart' as http;
 
 class SettingsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     final ipAddressProvider = Provider.of<IPAddressProvider>(context);
-    
+
+    Future<void> _sendHttpRequest(String selectedLanguage) async {
+    final ipAddressProvider = Provider.of<IPAddressProvider>(context, listen: false);
+    final ipAddress = ipAddressProvider.ipAddress;
+
+    final port = 5050;
+    final url = Uri.parse('http://$ipAddress:$port/select-language');
+
+    try {
+      final response = await http.post(url, body: {'text': selectedLanguage});
+
+      if (response.statusCode == 200) {
+        print('Text sent successfully!');
+      } else {
+        print('Failed to send text. Error: ${response.statusCode}');
+      }
+    } catch (error) {
+      print('Error: $error');
+    }
+  }
+
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 255, 255, 255),
       body: Stack(
@@ -64,6 +84,62 @@ class SettingsScreen extends StatelessWidget {
                     ),
                   ],
                 ),
+                SizedBox(height: 16),
+                // Add a button below the existing containers
+                ElevatedButton(
+                  onPressed: () {
+                    // Open a dialog here
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        String selectedLanguage = 'English'; // Default language
+
+                        return AlertDialog(
+                          title: Text('Select Language'),
+                          content: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              // Add a DropdownButton for language selection
+                              DropdownButton<String>(
+                                value: selectedLanguage,
+                                onChanged: (String? newValue) {
+                                  if (newValue != null) {
+                                    selectedLanguage = newValue;
+                                  }
+                                },
+                                items: ['English', 'Hindi', 'French']
+                                    .map<DropdownMenuItem<String>>(
+                                        (String value) {
+                                  return DropdownMenuItem<String>(
+                                    value: value,
+                                    child: Text(value),
+                                  );
+                                }).toList(),
+                              ),
+                            ],
+                          ),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: Text('Cancel'),
+                            ),
+                            TextButton(
+                              onPressed: () async {
+                                // Handle language selection here
+                                print(selectedLanguage);
+                                await _sendHttpRequest(selectedLanguage);
+                              },
+                              child: Text('Save'),
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  },
+                  child: Text('Open Dialog'),
+                ),
               ],
             ),
           ),
@@ -101,7 +177,6 @@ class SettingsScreen extends StatelessWidget {
     );
   }
 }
-
 
 class DeviceStatusContainer extends StatelessWidget {
   final IconData icon;
